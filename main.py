6c409bb68b990c61767b8bffd5702e2e0f10698e
Charlie Lamb <charlielamb20@icloud.com>
2025-02-21 09:43:36 +0000

    back to pydantic

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import os
+from typing import Optional
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
-from typing import Optional
-from typing_extensions import Annotated, TypedDict
 
 load_dotenv()
 
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+model = init_chat_model("gpt-3o-mini", model_provider="openai")
 
-model = init_chat_model("gpt-4o-mini", model_provider="openai")
-
-class Joke(TypedDict):
+# Pydantic
+class Joke(BaseModel):
     """Joke to tell user."""
 
-    setup: Annotated[str, ..., "The setup of the joke"] 
-    punchline: Annotated[str, ..., "The punchline of the joke"]
-    rating: Annotated[Optional[int], ..., "How funny the joke is, from 1 to 10"]
-    
+    setup: str = Field(description="The setup of the joke")
+    punchline: str = Field(description="The punchline to the joke")
+    rating: Optional[int] = Field(
+        default=None, description="How funny the joke is, from 1 to 10"
+    )
+
+
 structured_llm = model.with_structured_output(Joke)
 
-output = structured_llm.invoke("Tell me a joke about cats")
-
-print(output)
+structured_llm.invoke("Tell me a joke about cats")
